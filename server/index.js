06f8ea1c693354e0308aaa9c8345401777e9f975
aamirsatti-as <aamirsatti507@gmail.com>
2023-05-04 13:45:55 +0500

    updated

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser')
 const app = express();
 app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-
+// comment
 var corsOptions = {
     origin: true,
     credentials:  true
